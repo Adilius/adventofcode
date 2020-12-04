621b1f2bc8d9009cccdcfb1ce3912788abc9c427
Adilius <leugue-gamer@hotmail.com>
2020-12-04 19:15:38 +0100

    Day 3 part 2 done, day 4 part 1 done

---
+++ 2020/Day_4/part1.py
@@ -0,0 +1,35 @@
+input_file = open("input.txt","r")
+lines = input_file.readlines()
+
+#Create an array of passports stored in passportArray
+passport = []
+passportArray = []
+for line in lines:
+    #Create new passport
+    if line == '\n':
+        passportArray.append(passport)
+        passport = []
+    #Append new information to current passport
+    else:
+        line = line[:-1]
+        lineArray = line.split(' ')
+        for each in lineArray:
+            passport.append(each)
+
+def validatePassport(passport):
+    if len(passport) <= 6:
+        return False
+    if len(passport) == 8:
+        return True
+    if len(passport) == 7:
+        for each in passport:
+            if each[:3] == "cid":
+                return False
+        return True
+
+numberOfValidPassports = 0
+for i in range(len(passportArray)):
+    if validatePassport(passportArray[i]):
+        numberOfValidPassports += 1
+
+print("Number of valid passports:", numberOfValidPassports)
